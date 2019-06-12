7edc31f76385faac59672266b20b0ccbc55d2d86
Guillermo de la Puente <g.puente.allott@gmail.com>
2019-06-13 00:17:58 +0200

    Removed unnecessary style import

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createStore from './store';
 import App from './components/App';
-import './index.css';
 
 const store = createStore();
 
