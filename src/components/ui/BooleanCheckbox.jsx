f1c57dfb120436343810d8ac5b1e49822f58c8be
Guillermo de la Puente <g.puente.allott@gmail.com>
2019-08-03 19:43:37 +0200

    Improved checkbox styles

--- src/components/ui/BooleanCheckbox.jsx
+++ src/components/ui/BooleanCheckbox.jsx
@@ -1,13 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 1rem;
+  height: 1rem;
+  margin: 0 0.5rem -1px 0;
+`;
+const ImprovedCheckbox = styled.div`
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-style: solid;
+  border-color: ${props => props.theme.colors.border};
+  border-width: 1px;
+  background-color: ${props => (props.checked ? props.theme.buttons.primary.backgroundColor : 'transparent')};
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 0.3rem;
+    height: 0.6rem;
+    border-style: solid;
+    border-width: 0 2px 2px 0;
+    border-color: ${props => props.theme.buttons.primary.color};
+    transform: translate(-50%, -65%) rotate(40deg);
+  }
+`;
 const Input = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  opacity: 0;
   cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
+
+  &:focus + ${ImprovedCheckbox} {
+    outline: ${props => props.theme.colors.textHighlight} auto 5px;
+  }
 `;
 
 export default ({ onChange, value }) => {
   const onCheckboxChange = event => onChange(event, Boolean(event.target.checked));
   return (
-    <Input type="checkbox" value="1" checked={value} onChange={onCheckboxChange} />
+    <Container>
+      <Input type="checkbox" value="1" checked={value} onChange={onCheckboxChange} />
+      <ImprovedCheckbox checked={value} />
+    </Container>
   );
 };
