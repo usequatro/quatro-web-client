4792d79b3cc103db3a497e6883bcca0dc587df86
Mitul Patel <mitul@splashthat.com>
2020-02-18 18:09:19 -0500

    fix the scroll into view issue on editing or adding a new task

--- src/components/ui/TransparentInputField.jsx
+++ src/components/ui/TransparentInputField.jsx
@@ -51,7 +51,7 @@ const InputField = forwardRef((
         var val = e.target.value;
         e.target.value = '';
         e.target.value = val;
-        e.target.scrollIntoView();
+        e.target.closest('main').scrollIntoView();
       }}
 
       {...props} />
