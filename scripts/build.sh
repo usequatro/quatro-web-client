cbb85f1df2836af2a9e04c5383df91b19123257a
Guillermo de la Puente <g.puente.allott@gmail.com>
2020-07-06 23:17:59 +0200

    A few more tweaks to CD

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,10 +10,10 @@ BYellow='\033[1;33m'
 BRed='\033[1;31m'
 
 if [[ "$FIREBASE_ACTIVE_PROJECT" == "quatro-dev-88030" ]]; then
-    printf "Building for Firebase project ${BGreen}$FIREBASE_ACTIVE_PROJECT${Color_Off}\n";
+    printf "Building with build-dev for ${BGreen}$FIREBASE_ACTIVE_PROJECT${Color_Off}\n";
     npm run build-dev
 elif [[ "$FIREBASE_ACTIVE_PROJECT" == "tasket-project" ]]; then
-    printf "Building for Firebase project ${BYellow}$FIREBASE_ACTIVE_PROJECT${Color_Off}\n";
+    printf "Building with build-prod for ${BYellow}$FIREBASE_ACTIVE_PROJECT${Color_Off}\n";
     npm run build-prod
 else
     printf "Unknown Firebase project ${BRed}$FIREBASE_ACTIVE_PROJECT${Color_Off}\n";
