98098c0433630be0b7159bc562a20d4885704b98
Guillermo de la Puente <g.puente.allott@gmail.com>
2020-10-04 18:32:28 +0200

    Fix unit test

--- src/utils/getUserFacingRecurringText.test.js
+++ src/utils/getUserFacingRecurringText.test.js
@@ -18,69 +18,85 @@ describe('getUserFacingRecurringText', () => {
   });
 
   it('returns days', () => {
-    expect(getUserFacingRecurringText({
-      amount: 3,
-      unit: DAY,
-    })).toBe('Every 3 days');
-    expect(getUserFacingRecurringText({
-      amount: 1,
-      unit: DAY,
-    })).toBe('Every day');
+    expect(
+      getUserFacingRecurringText({
+        amount: 3,
+        unit: DAY,
+      }),
+    ).toBe('Every 3 days');
+    expect(
+      getUserFacingRecurringText({
+        amount: 1,
+        unit: DAY,
+      }),
+    ).toBe('Every day');
   });
 
   it('returns months', () => {
-    expect(getUserFacingRecurringText({
-      amount: 3,
-      unit: MONTH,
-    })).toBe('Every 3 months');
-    expect(getUserFacingRecurringText({
-      amount: 1,
-      unit: MONTH,
-    })).toBe('Every month');
+    expect(
+      getUserFacingRecurringText({
+        amount: 3,
+        unit: MONTH,
+      }),
+    ).toBe('Every 3 months');
+    expect(
+      getUserFacingRecurringText({
+        amount: 1,
+        unit: MONTH,
+      }),
+    ).toBe('Every month');
   });
 
   it('returns days of the week', () => {
-    expect(getUserFacingRecurringText({
-      amount: 1,
-      unit: WEEK,
-      activeWeekdays: {
-        [MONDAY]: true,
-        [TUESDAY]: true,
-        [WEDNESDAY]: true,
-      }
-    })).toBe('Every Monday, Tuesday and Wednesday');
-    expect(getUserFacingRecurringText({
-      amount: 2,
-      unit: WEEK,
-      activeWeekdays: {
-        [MONDAY]: true,
-        [TUESDAY]: true,
-        [WEDNESDAY]: true,
-      }
-    })).toBe('Every Monday, Tuesday and Wednesday every 2 weeks');
-    expect(getUserFacingRecurringText({
-      amount: 2,
-      unit: WEEK,
-      activeWeekdays: {
-        [MONDAY]: true,
-        [TUESDAY]: true,
-        [WEDNESDAY]: true,
-        [THURSDAY]: true,
-        [FRIDAY]: true,
-      }
-    })).toBe('Every weekday every 2 weeks');
-    expect(getUserFacingRecurringText({
-      amount: 1,
-      unit: WEEK,
-      activeWeekdays: {
-        [MONDAY]: true,
-        [TUESDAY]: true,
-        [WEDNESDAY]: true,
-        [THURSDAY]: true,
-        [FRIDAY]: true,
-        [SATURDAY]: true,
-        [SUNDAY]: true,
-      }
-    })).toBe('Every day');
+    expect(
+      getUserFacingRecurringText({
+        amount: 1,
+        unit: WEEK,
+        activeWeekdays: {
+          [MONDAY]: true,
+          [TUESDAY]: true,
+          [WEDNESDAY]: true,
+        },
+      }),
+    ).toBe('Every Monday, Tuesday and Wednesday');
+    expect(
+      getUserFacingRecurringText({
+        amount: 2,
+        unit: WEEK,
+        activeWeekdays: {
+          [MONDAY]: true,
+          [TUESDAY]: true,
+          [WEDNESDAY]: true,
+        },
+      }),
+    ).toBe('Every Monday, Tuesday and Wednesday every 2 weeks');
+    expect(
+      getUserFacingRecurringText({
+        amount: 2,
+        unit: WEEK,
+        activeWeekdays: {
+          [MONDAY]: true,
+          [TUESDAY]: true,
+          [WEDNESDAY]: true,
+          [THURSDAY]: true,
+          [FRIDAY]: true,
+        },
+      }),
+    ).toBe('Every weekday (Monday to Friday) every 2 weeks');
+    expect(
+      getUserFacingRecurringText({
+        amount: 1,
+        unit: WEEK,
+        activeWeekdays: {
+          [MONDAY]: true,
+          [TUESDAY]: true,
+          [WEDNESDAY]: true,
+          [THURSDAY]: true,
+          [FRIDAY]: true,
+          [SATURDAY]: true,
+          [SUNDAY]: true,
+        },
+      }),
+    ).toBe('Every day');
   });
 });
