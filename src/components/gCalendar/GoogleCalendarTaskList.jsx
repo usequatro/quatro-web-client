3e7cccba6bdc3fd581ccdc68b09f97c1887571ee
Alamedadev <hola@alamedadev.com>
2020-11-09 18:13:28 +0100

    Base for showing calentar task list Missing: render calentar task list once we have the connected calendars

--- src/components/gCalendar/GoogleCalendarTaskList.jsx
+++ src/components/gCalendar/GoogleCalendarTaskList.jsx
@@ -1,83 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+import { cond } from 'lodash';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
 
 import {
-  setDashboardActiveTab
-} from '../../modules/dashboard';
-
-import {
-  selectConnectedGoogleCalendars
+  selectConnectedGoogleCalendars,
 } from '../../modules/googleCalendar';
-import { cond } from 'lodash';
+import { GOOGLE_CALENDAR_TASK_LIST } from '../../constants/dashboardTabs';
+import EmptyState from '../dashboard/tasks/EmptyState';
 
 const useStyles = makeStyles(() => ({
   container: {
     paddingHorizontal: 90,
     paddingTop: 130,
-    display: "flex",
-    alignItems: "center",
-    alignContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'center',
     overflow: 'auto',
     backgroundColor: '#ffffff',
-    flexDirection: "column"
+    flexDirection: 'column'
   },
 }));
 
-
 const GoogleCalendarTaskList = () => {
   const classes = useStyles();
+  const history = useHistory();
   const connectedGoogleCalendars = useSelector(selectConnectedGoogleCalendars);
 
-  console.log('connectedGoogleCalendars', connectedGoogleCalendars)
+  const showGoogleCalendarList = () => {
+    history.push("/dashboard/google-calendar-list");  
+  };
+
+  const renderCalendarTaskList = () => {
+    connectedGoogleCalendars.map((cal, index) => {
+      return (
+        <div key={index}><p>Calendar List</p></div>
+      )
+    })
+  }
 
   return (
     <Box className={classes.container}>
       {cond([
-        [() => connectedGoogleCalendars.length === 0 , () => <ConnectCalendar />],
-      ])}
+         [() => connectedGoogleCalendars.length === 0, () => (
+            <>
+              <EmptyState tab={GOOGLE_CALENDAR_TASK_LIST} />
+              <Button onClick={() => showGoogleCalendarList()} variant="contained">Connect Calendar</Button>
+            </>
+         )],
+       ])}
     </Box>
   )
 };
 
+// [() => true, () => { renderCalendarTaskList() }]
+
 export default GoogleCalendarTaskList;
-
-const ConnectCalendar = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const classes = useStyles();
-
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const openModal = () => {
-    dispatch(setDashboardActiveTab("/dashboard/backlog"));
-    history.push("/dashboard/backlog");
-  }
-  const closeModal = () => {
-    setModalVisible(false)
-  }
-  const renderConnectCalendarList = () => {
-    return(
-      <Box className={classes.container}>
-        <p>My modal</p>
-      </Box>
-    )
-  }
-  return (
-    <Box className={classes.container}>
-      {/* <Typography>
-      You haven`t connected any calendar.
-      </Typography> */}
-      <Button onClick={() => openModal()} variant="contained">Connect Calendar</Button>
-      <Modal open={modalVisible} onClose={() => closeModal()}>
-        {renderConnectCalendarList()}
-      </Modal> 
-    </Box>
-  )
-}
