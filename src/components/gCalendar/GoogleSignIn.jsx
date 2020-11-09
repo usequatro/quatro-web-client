e76899f88fb86e72a9eb0f8eb9da9f4e5c83e2c8
Alamedadev <hola@alamedadev.com>
2020-11-09 10:06:29 +0100

    Screen for sign-in in to Google

---
+++ src/components/gCalendar/GoogleSignIn.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import * as dashboardTabs from '../../constants/dashboardTabs';
+import EmptyState from '../dashboard/tasks/EmptyState';
+
+import {
+  selectGoogleAPIClient,
+} from '../../modules/googleCalendar';
+
+const tab = dashboardTabs.GOOGLE_CALENDAR
+
+const useStyles = makeStyles(() => ({
+  container: {
+    flexGrow: 1,
+    padding: 90,
+    display: "flex",
+    alignItems: "center",
+    alignContent: "center",
+    border: 'solid 1px rgba(0, 0, 0, 0.12)',
+    resize: 'horizontal',
+    overflow: 'auto',
+    backgroundColor: '#ffffff',
+    flexDirection: "column"
+  },
+}));
+
+const GoogleSignIn = () => {
+  const classes = useStyles();
+  const googleAPIClient = useSelector(selectGoogleAPIClient);
+
+
+  const connectGoogle = () => {
+    googleAPIClient.auth2.getAuthInstance().signIn();
+  };
+
+  return (
+    <Box className={classes.container}>
+      <EmptyState tab={tab} />
+      <Button onClick={() => connectGoogle()} variant="contained">Connect Calendar</Button>
+    </Box>
+  )
+};
+
+export default GoogleSignIn;
