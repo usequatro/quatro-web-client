0c9c5061a9e85be74ad2cad703b36f57cc122299
Minacava <mcamachovarea@gmail.com>
2020-12-10 19:32:35 +0100

    When viewing the calendar, I should see the tasks from this time onward.

--- src/components/gCalendar/CurrentTime.jsx
+++ src/components/gCalendar/CurrentTime.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import moment from 'moment';
 
 import { useStyles } from './sharedStyles';
 
 const CurrentTime = () => {
+  const currentTimeRef = useRef();
   const classes = useStyles();
+
   const differenceWithEndOfDay = moment(moment().endOf('day').toISOString()).diff(moment());
   const differenceMiliSecs = moment.duration(differenceWithEndOfDay, 'milliseconds');
   const differenceDuration = Math.floor(differenceMiliSecs.asMinutes() / 15);
   const topDifferenceTicks = differenceDuration + 2;
   const top = -Math.abs(topDifferenceTicks * 25);
 
-  return <div style={{ top }} className={classes.currentTime} id="currentTime" />;
+  useEffect(() => {
+    if (currentTimeRef && currentTimeRef.current) {
+      currentTimeRef.current.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    }
+  }, [currentTimeRef]);
+
+  return (
+    <div ref={currentTimeRef} style={{ top }} className={classes.currentTime} id="currentTime" />
+  );
 };
 
 export default CurrentTime;
