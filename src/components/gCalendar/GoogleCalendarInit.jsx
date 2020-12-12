669697aa6e0dd2890abef387f06285b7d4c2f671
Guillermo de la Puente <g.puente.allott@gmail.com>
2020-12-12 13:50:50 +0100

    Add ignore for console log warning

--- src/components/gCalendar/GoogleCalendarInit.jsx
+++ src/components/gCalendar/GoogleCalendarInit.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import {
-  selectUserId,
-} from '../../modules/session';
+import { selectUserId } from '../../modules/session';
 
 import {
   setGoogleAPIClient,
@@ -15,9 +13,9 @@ import {
 } from '../../modules/googleCalendar';
 
 const config = {
-  "clientId": process.env.REACT_APP_GOOGLE_CLIENT_ID,
-  "apiKey": process.env.REACT_APP_GOOGLE_API_KEY,
-  "scope": process.env.REACT_APP_GOOGLE_SCOPE,
+  clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  scope: process.env.REACT_APP_GOOGLE_SCOPE,
 };
 
 const GoogleCalendarInit = () => {
@@ -35,44 +33,40 @@ const GoogleCalendarInit = () => {
     [dispatch, googleAPIClient, userId],
   );
 
-  const initGoogleClient = useCallback(
-    () => {
-      googleAPIClient.client.init(config)
-        .then(() => {
-          // Listen for sign-in state changes.
-          googleAPIClient.auth2.getAuthInstance().isSignedIn.listen(
-            state => updateSignInStatus(state));
-          // Handle the initial sign-in state.
-          updateSignInStatus(googleAPIClient.auth2.getAuthInstance().isSignedIn.get());
-        })
-        .catch((e) => {
-          console.log("ERROR on initGoogleClient: ", e);
-        });
-      googleAPIClient.client.load('calendar','v3');
-    },
-    [googleAPIClient, updateSignInStatus],
-  );
+  const initGoogleClient = useCallback(() => {
+    googleAPIClient.client
+      .init(config)
+      .then(() => {
+        // Listen for sign-in state changes.
+        googleAPIClient.auth2
+          .getAuthInstance()
+          .isSignedIn.listen((state) => updateSignInStatus(state));
+        // Handle the initial sign-in state.
+        updateSignInStatus(googleAPIClient.auth2.getAuthInstance().isSignedIn.get());
+      })
+      .catch((e) => {
+        console.error('ERROR on initGoogleClient: ', e); // eslint-disable-line no-console
+      });
+    googleAPIClient.client.load('calendar', 'v3');
+  }, [googleAPIClient, updateSignInStatus]);
 
-  const injectGoogleAPIScript = useCallback(
-    () => {
-      if (googleAPIClient) {
-        googleAPIClient.load('client:auth2', initGoogleClient);
-      } else {
-        const gScript = document.createElement('script');
-        gScript.type = 'text/javascript';
-        gScript.src = 'https://apis.google.com/js/platform.js?onload=onScriptLoad';
-        document.body.appendChild(gScript);
-        gScript.onload = () => {
-          dispatch(setGoogleAPIClient(window.gapi));
-        };
-      }
-    },
-    [dispatch, googleAPIClient, initGoogleClient],
-  )
+  const injectGoogleAPIScript = useCallback(() => {
+    if (googleAPIClient) {
+      googleAPIClient.load('client:auth2', initGoogleClient);
+    } else {
+      const gScript = document.createElement('script');
+      gScript.type = 'text/javascript';
+      gScript.src = 'https://apis.google.com/js/platform.js?onload=onScriptLoad';
+      document.body.appendChild(gScript);
+      gScript.onload = () => {
+        dispatch(setGoogleAPIClient(window.gapi));
+      };
+    }
+  }, [dispatch, googleAPIClient, initGoogleClient]);
 
   useEffect(() => {
     injectGoogleAPIScript();
-  },[injectGoogleAPIScript]);
+  }, [injectGoogleAPIScript]);
 
   return null;
 };
