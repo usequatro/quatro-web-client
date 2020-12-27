e4aea2ad17c20413508aaa64ac1cfa242eb86dc7
Guillermo de la Puente <g.puente.allott@gmail.com>
2020-12-27 16:54:57 +0100

    Improve CI/CD workflow with master CD to dev and releases with tags. Improve README

--- scripts/notifyDeploy.sh
+++ scripts/notifyDeploy.sh
@@ -3,9 +3,12 @@
 # Notifies that the deploy has completed
 
 if [[ "$GCLOUD_PROJECT" == "quatro-dev-88030" ]]; then
-    ./scripts/messageSlack.sh "Deployer" "Application deployed - DEVELOPMENT - https://dev.usequatro.com 🛶"
+    LATEST_COMMIT=$(git log --pretty=format:"%h - %an, %ar: \\\"%s\\\"" -1);
+    ./scripts/messageSlack.sh "Dev Deployer 🛶" "Application deployed - https://dev.usequatro.com - ${LATEST_COMMIT}"
 elif [[ "$GCLOUD_PROJECT" == "tasket-project" ]]; then
-    ./scripts/messageSlack.sh "Deployer" "Application deployed - PRODUCTION - https://app.usequatro.com 🚀"
+    LATEST_COMMIT=$(git log --pretty=format:"%h - %an, %ar: \\\"%s\\\"" -1);
+    LATEST_TAG=$(git describe --abbrev=0 --tags);
+    ./scripts/messageSlack.sh "Production Deployer 🚀" "Application deployed - https://app.usequatro.com - ${LATEST_COMMIT} - Tag: ${LATEST_TAG}"
 else
     printf "Unknown Firebase project ${BRed}$GCLOUD_PROJECT${Color_Off}\n";
     exit 1;
