22374a2eee18f561e329292060c1dc10ec6d21ea
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-01-17 18:14:37 +0100

    Lower-case 'password' in Mixpanel property

--- src/constants/mixpanelUserProperties.js
+++ src/constants/mixpanelUserProperties.js
@@ -1,2 +1,2 @@
-export const SIGNED_IN_WITH_PASSWORD = 'Signed in with Password';
+export const SIGNED_IN_WITH_PASSWORD = 'Signed in with password';
 export const SIGNED_IN_WITH_GOOGLE = 'Signed in with Google';
