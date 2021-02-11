e24754f1f6b92de3b2dc51655179a844d3b101ad
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-02-11 20:35:37 +0100

    Fix issue with parsing links with character before link and when link was the start of the string

--- src/utils/parseHtml.js
+++ src/utils/parseHtml.js
@@ -5,9 +5,14 @@ const removeLinkTarget = (html) => html.replace(/<a\s(.+)(target="[\s_a-z]")(.+)
 const removeLinkRel = (html) => html.replace(/<a\s(.+)(rel="[\s_a-z]")(.+)>/gi, '<a $1$2>');
 const addLinkAttrs = (html) =>
   html.replace(/<a\s([^>]+)>/gi, '<a $1 target="_blank" rel="noopener noreferrer">');
+
+const urlRegexp = "https?:\\/\\/[A-Za-z0-9-._~:/?#[\\]@!$&'()*+,;%=]*";
+const doubleQuotesLookahead = '(?=([^"]*"[^"]*")*[^"]*$)';
+const urlsNotWrappedInQuotes = new RegExp(`(${urlRegexp})${doubleQuotesLookahead}`);
+
 const addLinksToPlainUrls = (html) =>
   html.replace(
-    /[^"](https?:\/\/[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;%=]*)/,
+    urlsNotWrappedInQuotes,
     '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>',
   );
 
--- src/utils/parseHtml.test.js
+++ src/utils/parseHtml.test.js
@@ -6,8 +6,40 @@ describe('parseHtml', () => {
   });
 
   it('should add attributes to links', () => {
-    expect(parseHtml('hola amigo <a href="example.com">Dani</a>!')).toBe(
-      'hola amigo <a href="example.com" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">Dani</a>!',
-    );
+    const input = 'hola amigo <a href="http://example.com">Dani</a>!';
+    const output =
+      'hola amigo <a href="http://example.com" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">Dani</a>!';
+
+    expect(parseHtml(input)).toBe(output);
+  });
+
+  it('should add attributes to links when they are alone', () => {
+    const input = '<a href="example.com">Dani</a>';
+    const output =
+      '<a href="example.com" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">Dani</a>';
+
+    expect(parseHtml(input)).toBe(output);
+  });
+
+  it('should add an A tag around regular links', () => {
+    const input = 'Check out https://quatro.com today';
+    const output =
+      'Check out <a href="https://quatro.com" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">https://quatro.com</a> today';
+    expect(parseHtml(input)).toBe(output);
+  });
+
+  it('should add an A tag around regular links with quary params', () => {
+    const input = 'Check out https://quatro.com?faq today';
+    const output =
+      'Check out <a href="https://quatro.com?faq" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">https://quatro.com?faq</a> today';
+    expect(parseHtml(input)).toBe(output);
+  });
+
+  it('should add an A tag around regular links when they are alone', () => {
+    const input =
+      'https://docs.google.com/document/d/1z9uK2gFBZeuiMLmsh08kf9RSmdj7RKzR1OC20U-m_cI/edit#heading=h.dvnefaql60r3';
+    const output =
+      '<a href="https://docs.google.com/document/d/1z9uK2gFBZeuiMLmsh08kf9RSmdj7RKzR1OC20U-m_cI/edit#heading=h.dvnefaql60r3" target="_blank" rel="noopener noreferrer" class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary">https://docs.google.com/document/d/1z9uK2gFBZeuiMLmsh08kf9RSmdj7RKzR1OC20U-m_cI/edit#heading=h.dvnefaql60r3</a>';
+    expect(parseHtml(input)).toBe(output);
   });
 });
