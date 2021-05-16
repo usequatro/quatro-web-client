9b9f19f09d405b99294cd680839ca317425ba69b
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-05-16 19:44:29 +0200

    Receive desktop version with postMessage from nativefied desktop app

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './firebase';
 
 import App from './components/App';
+import debugConsole from './utils/debugConsole';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -13,3 +14,21 @@ window.quatro = {
   appEnv: process.env.REACT_APP_DEVELOPMENT ? 'development' : 'production',
   emulator: process.env.REACT_APP_FIREBASE_EMULATOR,
 };
+
+// Receive messages from the native application wrapper
+window.addEventListener('message', (event) => {
+  if (event.origin !== window.origin) {
+    return;
+  }
+  if (event.data) {
+    try {
+      const data = JSON.parse(event.data);
+      debugConsole.log('Message', data);
+      if (data.desktopClientVersion) {
+        window.quatro.desktopClientVersion = data.desktopClientVersion;
+      }
+    } catch (error) {
+      console.error(error); // eslint-disable-line no-console
+    }
+  }
+});
