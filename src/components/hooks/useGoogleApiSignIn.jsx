f6fb30ad24c8ab3edb09bf86192a8c147f0cac23
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-06-21 10:35:00 -0400

    Make redirect url absolute

--- src/components/hooks/useGoogleApiSignIn.jsx
+++ src/components/hooks/useGoogleApiSignIn.jsx
@@ -60,7 +60,7 @@ export default function useGoogleApiSignIn() {
         // @link https://developers.google.com/identity/sign-in/web/reference#googleauthsignin
         .signIn({
           ux_mode: 'redirect',
-          redirect_uri: 'dashboard/calendars?googleconnected=1',
+          redirect_uri: `${window.location.origin}/dashboard/calendars?googleconnected=1`,
         })
     );
   }, []);
