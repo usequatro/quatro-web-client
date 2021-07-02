d348b121b8bbd4f23fd6ff641c69380c9b72e139
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-07-02 13:36:00 -0400

    Detect if app is running with todesktop

--- src/utils/applicationClient.js
+++ src/utils/applicationClient.js
@@ -2,17 +2,17 @@
  * @returns {bool} - true if using from a web browser
  */
 export function isClientWeb() {
-  return window.quatro.desktopClientVersion === undefined;
+  return window.quatro.desktopClientVersion === undefined && window.todesktop.version === undefined;
 }
 /**
  * @returns {bool} - true if using from the Quatro desktop client
  */
 export function isClientDesktop() {
-  return window.quatro.desktopClientVersion !== undefined;
+  return window.quatro.desktopClientVersion !== undefined || window.todesktop.version !== undefined;
 }
 /**
  * @returns {string|undefined} - string with version if using from the Quatro desktop client
  */
 export function getClientDesktopVersion() {
-  return window.quatro.desktopClientVersion;
+  return window.quatro.desktopClientVersion || window.todesktop.version;
 }
