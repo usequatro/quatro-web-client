7c6b287e75d9dbe937b561c482679227729fd9a7
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-07-02 13:55:15 -0400

    Fix undefined behaviour

--- src/utils/applicationClient.js
+++ src/utils/applicationClient.js
@@ -2,17 +2,17 @@
  * @returns {bool} - true if using from a web browser
  */
 export function isClientWeb() {
-  return window.quatro.desktopClientVersion === undefined && window.todesktop.version === undefined;
+  return window.quatro.desktopClientVersion === undefined && window.todesktop === undefined;
 }
 /**
  * @returns {bool} - true if using from the Quatro desktop client
  */
 export function isClientDesktop() {
-  return window.quatro.desktopClientVersion !== undefined || window.todesktop.version !== undefined;
+  return window.quatro.desktopClientVersion !== undefined || window.todesktop !== undefined;
 }
 /**
  * @returns {string|undefined} - string with version if using from the Quatro desktop client
  */
 export function getClientDesktopVersion() {
-  return window.quatro.desktopClientVersion || window.todesktop.version;
+  return window.quatro.desktopClientVersion || (window.todesktop && window.todesktop.version);
 }
